b49e822f3c4bd6513ae590bd2e29eebf6d2b795d
Ishika Agarwal <86250466+ishika024@users.noreply.github.com>
2023-07-24 23:38:36 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,4 +1,4 @@
-//Prime Number :
+//Prime Number : A prime number is a whole number greater than 1 whose only factors are 1 and itself. A factor is a whole number that can be divided evenly into another number.
 
  
 import java.util.Scanner;
