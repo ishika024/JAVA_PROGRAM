13e6d13281a57d0fe715bf7b6ee6114047331084
Ishika Agarwal <86250466+ishika024@users.noreply.github.com>
2023-07-24 23:37:43 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,37 @@
+//Prime Number :
+
+ 
+import java.util.Scanner;
+public class Main 
+{  
+   public static void main(String[] args) 
+   {  
+       //Take input from the user
+       //Create instance of the Scanner class
+       Scanner sc = new Scanner(System.in);  
+       System.out.println("Enter a number : ");  
+       int num = sc.nextInt();  
+       int flag=0; 
+       int i, temp=num/2;      
+       if(num==0||num==1)
+       {  
+          System.out.println(num+" is not prime number");      
+       }
+       else
+       {  
+         for(i=2;i<=temp;i++)
+         {      
+           if(num%i==0)
+           {      
+               System.out.println(num+" is not a prime number");      
+               flag=1;      
+               break;      
+           }      
+         }      
+         if(flag==0)  
+         { 
+             System.out.println(num+" is a prime number");  
+         }  
+        }//end of else  
+   }   
+}  
